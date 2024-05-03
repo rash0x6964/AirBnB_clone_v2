49bb11f6f785f548473ae90b99b3a9374fbad9d7
rash0x6964 <rash0x6964@gmail.com>
2024-05-03 22:50:16 +0100

    Keep it clean!

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Fabfile to delete out-of-date archives."""
+import os
+from fabric.api import *
+
+env.hosts = ['54.236.54.151	', '34.229.189.161']
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives."""
+
+    number = int(number)
+    if number < 1:
+        number = 1
+
+    versions_dir = 'versions/'
+    archives = sorted(os.listdir(versions_dir), reverse=True)
+    for archive in archives[number:]:
+        local('rm versions/{}'.format(archive))
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
