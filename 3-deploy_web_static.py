e5b6aa78793b4dae63f1290e90e2430d689116e2
rash0x6964 <rash0x6964@gmail.com>
2024-05-03 21:14:00 +0100

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""Generates a .tgz archive from the contents of the web_static."""
+"""Creates and distributes an archive to your web servers."""
 import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
+from fabric.api import local
+from datetime import datetime
 
 
 env.hosts = ['54.236.54.151	', '34.229.189.161']
+
+def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    if local("tar -cvzf {} web_static".format(archive_path)).failed is True:
+        return None
+    return archive_path
+
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
@@ -34,3 +50,10 @@ def do_deploy(archive_path):
         return True
     except Exception as e:
         return False
+
+def deploy():
+    """Create and distribute an archive to a web server."""
+    file = do_pack()
+    if file is None:
+        return False
+    return do_deploy(file)
