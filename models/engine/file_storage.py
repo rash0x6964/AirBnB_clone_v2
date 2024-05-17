80036c43242c2d84a4a054836b26abaf652cf875
rash0x6964 <rash0x6964@gmail.com>
2024-05-17 10:54:40 +0100

    fix some checker issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,8 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+            return {k: v for k, v in self.__objects.items()
+                    if isinstance(v, cls)}
         return self.__objects
 
     def new(self, obj):
