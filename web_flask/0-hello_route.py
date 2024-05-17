11007741e13935beb97cb57d1ee800ee4addc49a
rash0x6964 <rash0x6964@gmail.com>
2024-05-17 11:01:58 +0100

    change file premission

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
